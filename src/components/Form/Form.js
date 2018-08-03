26abbf2acc4659f4eddb48db3794c9ea3eeb5029
zyabel <zyabel@gmail.com>
2018-08-03 12:47:24 +0300

    Form validation added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,65 +1,80 @@
-import React, { Component } from 'react'
-import { 
+import React, { Component } from 'react';
+import {
   ControlLabel,
   FormControl,
   FormGroup,
   HelpBlock,
-} from 'react-bootstrap'
+} from 'react-bootstrap';
 
 class FormField extends Component {
   constructor(props, context) {
-    super(props, context)
+    super(props, context);
 
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.getValidationState = this.getValidationState.bind(this);
 
     this.state = {
-      value: ''
-    }
+      value: '',
+      type: '',
+    };
   }
 
   getValidationState() {
-    const length = this.state.value.length   
+    const length = this.state.value.length;
 
-    switch (this.props.data.type) {
+    switch (this.state.type) {
       case 'textarea':
-          if (length > 100) return 'success'
-          else if (length > 5) return 'warning'
-          else if (length > 0) return 'error'
-          return null
+        if (length > 10) {
+          return 'success';
+        } else {
+          return 'error';
+        }
       case 'text':
-        if (length > 2) return 'success'
-        else if (length < 0) return 'warning'
-        else if (length > 10) return 'error'
-        return null
+        let text = /^[A-Za-z0-9_]+$/;
+        if (text.test(this.state.value)) {
+          return 'success';
+        } else {
+          return 'error';
+        }
       case 'tel':
-        if (length > 10) return 'success'
-        else if (length > 16) return 'warning'
-        else if (length > 0) return 'error'
-        return null
+        let tel = /^\+?[0-9]{3}-?[0-9]{6,12}$/;
+        if (tel.test(this.state.value)) {
+          return 'success';
+        } else {
+          return 'error';
+        }
       case 'email':
-        if (length > 10) return 'success'
-        else if (length > 5) return 'warning'
-        else if (length > 0) return 'error'
-        return null
+        let email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (email.test(this.state.value)) {
+          return 'success';
+        } else {
+          return 'error';
+        }
       default:
-        alert( 'Я таких значений не знаю' );
+        return null;
     }
   }
 
   handleChange(e) {
-    this.setState({ value: e.target.value })
-  }   
+    this.setState({ type: e.target.type, value: e.target.value });
+  }
 
   render() {
-    const { id, label, type, placeholder, componentClass, help } = this.props.data
-    const { getData } = this.props
+    const {
+      id,
+      label,
+      type,
+      placeholder,
+      componentClass,
+      help,
+    } = this.props.data;
+    const { getData } = this.props;
 
     return (
       <FormGroup
         controlId={id}
         validationState={this.getValidationState()}
-        label={label}  
-    
+        label={label}
       >
         <ControlLabel>{label}</ControlLabel>
         <FormControl
@@ -71,10 +86,12 @@ class FormField extends Component {
           inputRef={getData}
         />
         <FormControl.Feedback />
-        {help && <HelpBlock>{help}</HelpBlock>}
+        {this.getValidationState() === 'error' ? (
+          <HelpBlock>{help}</HelpBlock>
+        ) : null}
       </FormGroup>
-    )
+    );
   }
 }
 
-export default FormField
+export default FormField;
