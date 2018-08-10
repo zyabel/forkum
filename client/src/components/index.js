fc39a90db3b93280ca0ea8f52e0f4184d7bc250a
zyabel <zyabel@gmail.com>
2018-08-10 10:39:05 +0300

    Data from DB loaded

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,4 @@ export { default as MapComponent } from './Map/Map';
 export { default as Card } from './Card/Card';
 export { default as Pagination } from './Pagination/Pagination';
 export { default as Spinner } from './Spinner/Spinner';
+export { default as Form } from './Form/Form';
--- client/src/redux/sagas/page_Home.js
+++ client/src/redux/sagas/page_Home.js
@@ -4,14 +4,16 @@ import {
   PAGE_HOME_GETDATA_REQUEST,
   PAGE_HOME_GETDATA_FAILURE,
 } from '../constants';
-import Api from '../Api';
+// import Api from '../Api';
+import TransferData from '../Api/transferData';
 
 function* pageHomeGetDataAsync(action) {
   try {
-    const pageData = yield call(Api.dataLocalHomePage);
-
-    yield put({ type: PAGE_HOME_GETDATA_SUCCESS, payload: pageData });
+    const pageData = yield call(TransferData.data);
+    console.log('saga', pageData);
+    yield put({ type: PAGE_HOME_GETDATA_SUCCESS, payload: pageData.Slides });
   } catch (e) {
+    console.log('sagaERROR');
     yield put({ type: PAGE_HOME_GETDATA_FAILURE });
   }
 }
