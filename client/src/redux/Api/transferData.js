3b86154188f4f63c65eeb854f8fe1d1a45bbe441
zyabel <zyabel@gmail.com>
2018-08-10 12:46:32 +0300

    Minor code changes

---
+++ client/src/redux/Api/transferData.js
@@ -0,0 +1,46 @@
+class TransferData {
+  // Getting page data for the Home page
+  static dataHomePage() {
+    return fetch('http://localhost:8000/')
+      .then((response) => {
+        if (response.status !== 200) {
+          console.log(`Oops, problem. Status Code: ${response.status}`);
+          return;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      });
+  }
+
+  // Getting page data for the cabinet/user page
+  static dataServerUserCabinet() {
+    return fetch('http://localhost:8080/cabinet/user')
+      .then((response) => {
+        if (response.status !== 200) {
+          console.log(`Oops, problem. Status Code: ${response.status}`);
+          return;
+        }
+        return response.json();
+      })
+      .then((data) => { return data; });
+  }
+
+  // Dispathing form data for the cabinet/user page
+  static personalDataSendToServer(data) {
+    return fetch('http://localhost:8080/cabinet/user/metrics', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ ...data }),
+    })
+      .then((response) => {
+        return response.json();
+      });
+  }
+}
+
+export default TransferData;
